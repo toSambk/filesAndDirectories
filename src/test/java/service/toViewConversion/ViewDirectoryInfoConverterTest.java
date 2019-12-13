d9184855b23d0100ae8653dff57e290afa6be60e
toSamBk <bk.simon50@gmail.com>
2019-12-14 01:22:24 +0300

    tests partially done fix

--- src/test/java/service/toViewConversion/ViewDirectoryInfoConverterTest.java
+++ src/test/java/service/toViewConversion/ViewDirectoryInfoConverterTest.java
@@ -1,13 +1,12 @@
 package service.toViewConversion;
 
 import config.TestConfig;
-import filesdirectories.service.toViewConversion.ByteConverter;
-import filesdirectories.service.builder.DirectoryBuilder;
-import filesdirectories.service.toViewConversion.ViewDirectoryInfoConverter;
 import filesdirectories.entities.Directory;
 import filesdirectories.exceptions.CannotReachFileAttributes;
+import filesdirectories.service.builder.DirectoryBuilder;
+import filesdirectories.service.toViewConversion.ByteConverter;
+import filesdirectories.service.toViewConversion.ViewDirectoryInfoConverter;
 import filesdirectories.viewRepresentation.DirectoryInfo;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +15,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class ViewDirectoryInfoConverterTest {
+public class ViewDirectoryInfoConverterTest {
 
     @Autowired
     private DirectoryBuilder directoryBuilder;
@@ -34,16 +29,13 @@ class ViewDirectoryInfoConverterTest {
     @Autowired
     private ViewDirectoryInfoConverter viewDirectoryInfoConverter;
 
-    @Autowired
-    private ByteConverter converter;
-
     private String sep = File.separator;
 
     @Test
     public void successfulBuild() {
         String path = "src" + sep + "test" + sep + "resources" + sep + "testFolder";
         File file = new File(path);
-        path = file.getAbsolutePath();
+        path = file.getPath();
         Directory built = directoryBuilder.build(file);
 
         assertNotNull(built);
@@ -55,7 +47,7 @@ class ViewDirectoryInfoConverterTest {
             DirectoryInfo result = viewDirectoryInfoConverter.convert(built);
             assertNotNull(result);
             assertEquals(1, result.getNumberOfDirectories());
-            assertEquals(1, result.getNumberOfFiles());
+            assertEquals(2, result.getNumberOfFiles());
         } catch (CannotReachFileAttributes e) {
             fail();
         }
--- src/test/java/service/toViewConversion/comparators/ComparatorsTest.java
+++ src/test/java/service/toViewConversion/comparators/ComparatorsTest.java
@@ -5,15 +5,16 @@ import filesdirectories.service.toViewConversion.comparators.ComparatorByName;
 import filesdirectories.service.toViewConversion.comparators.ComparatorByNumbersInFilename;
 import filesdirectories.viewRepresentation.FileInfo;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import java.util.Arrays;
+import java.util.List;
 
-import java.util.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
@@ -27,51 +28,74 @@ public class ComparatorsTest {
     @Autowired
     private ComparatorByName comparatorByName;
 
-    List<FileInfo> testList;
+    private List<FileInfo> testList1;
+
+    private List<FileInfo> testList2;
 
     @Before
     public void setup() {
+        testList1 = Arrays.asList(
+                new FileInfo("abc", "125", 1, true),        //0
+                new FileInfo("a123bc", "125", 1, true),     //1
+                new FileInfo("a10bc", "125", 1, true),      //2
+                new FileInfo("123456789", "125", 1, true),  //3
+                new FileInfo("a123bc523", "125", 1, true),  //4
+                new FileInfo("a123bc123", "125", 1, true),  //5
+                new FileInfo("bcbga10bc456", "125", 1, true),//6
+                new FileInfo("bcbga10bc", "125", 1, true)   //7
+        );
 
-        testList = Arrays.asList(
-                new FileInfo("abc", "125", 1, true),
-                new FileInfo("a123bc", "125", 1, true),
-                new FileInfo("a10bc", "125", 1, true),
-                new FileInfo("123456789", "125", 1, true),
-                new FileInfo("a123bc523", "125", 1, true),
-                new FileInfo("a123bc123", "125", 1, true),
-                new FileInfo("bcbga10bc456", "125", 1, true),
-                new FileInfo("bcbga10bc", "125", 1, true)
-
-                                                                            );
+        testList2 = Arrays.asList(
+                new FileInfo("abc", "125", 1, true),        //0
+                new FileInfo("bc", "125", 1, true),     //1
+                new FileInfo("cd", "125", 1, true),      //2
+                new FileInfo("de", "125", 1, true),  //3
+                new FileInfo("ef", "125", 1, true),  //4
+                new FileInfo("fg", "125", 1, true),  //5
+                new FileInfo("gh", "125", 1, true),//6
+                new FileInfo("hi", "125", 1, true)   //7
+        );
     }
 
     @Test
     public void sortingTestInNumbers() {
-
-        testList.sort(comparatorByNumbersInFilename);
-        testList.forEach(x-> System.out.println(x.getFilename()));
+        testList1.sort(comparatorByNumbersInFilename);
+        assertEquals("abc", testList1.get(0).getFilename());
+        assertEquals("a123bc123", testList1.get(5).getFilename());
+        assertEquals("a123bc523", testList1.get(6).getFilename());
+        assertEquals("123456789", testList1.get(7).getFilename());
 
     }
 
     @Test
     public void sortingTestAlphabeticallyAndNumbers() {
-        testList.sort(comparatorByName.thenComparing(comparatorByNumbersInFilename));
-        testList.forEach(x-> System.out.println(x.getFilename()));
+        testList1.sort(comparatorByName.thenComparing(comparatorByNumbersInFilename));
+        testList1.forEach(x -> System.out.println(x.getFilename()));
     }
 
 
     @Test
     public void sortingTestAlphabetically() {
-        testList.sort(comparatorByName);
-        testList.forEach(x-> System.out.println(x.getFilename()));
+        testList2.sort(comparatorByName);
+        assertEquals("abc", testList2.get(0).getFilename());
+        assertEquals("fg", testList2.get(5).getFilename());
+        assertEquals("gh", testList2.get(6).getFilename());
+        assertEquals("hi", testList2.get(7).getFilename());
     }
 
 
     @Test
-    @Ignore
     public void sortingTestNumbersAndAlphabetically() {
-        testList.sort(comparatorByNumbersInFilename.thenComparing(comparatorByName));
-        testList.forEach(x-> System.out.println(x.getFilename()));
+        testList1.sort(comparatorByName.thenComparing(comparatorByNumbersInFilename));
+        testList1.forEach(x-> System.out.println(x.getFilename()));
+        assertEquals("123456789", testList1.get(0).getFilename());
+        assertEquals("abc", testList1.get(1).getFilename());
+        assertEquals("a10bc", testList1.get(2).getFilename());
+        assertEquals("a123bc", testList1.get(3).getFilename());
+        assertEquals("a123bc123", testList1.get(4).getFilename());
+        assertEquals("a123bc523", testList1.get(5).getFilename());
+        assertEquals("bcbga10bc", testList1.get(6).getFilename());
+        assertEquals("bcbga10bc456", testList1.get(7).getFilename());
     }
 
 
